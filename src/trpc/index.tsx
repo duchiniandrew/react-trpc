385833f411e811afc16464e9f8305cc8c4b8e352
Andrew Duchini <97451223+andrewoliveiraduchini@users.noreply.github.com>
2024-08-23 11:45:28 -0300

    fixed path and port

--- src/trpc/index.tsx
+++ src/trpc/index.tsx
@@ -1,10 +1,10 @@
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
-import { AppRouter } from '../../../nest-trpc/src/modules/trpc/router/trpc.router'
+import { AppRouter } from '../../../mamba-project/src/trpc/trpc.router'
 
 export const trpc = createTRPCProxyClient<AppRouter>({
     links: [
         httpBatchLink({
-            url: "http://localhost:4000/trpc", // you should update this to use env variables
+            url: "http://localhost:3000/trpc", // you should update this to use env variables
         }),
     ],
 });
